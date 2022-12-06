f3e84a0f9d7afd50c0e3cf2d0d437d72fbdfbc0c
Pawee Tantivasdakarn <mewpawee@Pawees-Mac-mini.local>
2022-12-06 23:47:30 +0700

    feat: initial commit

---
+++ test/Token.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Token.sol";
+
+contract TokenTest is Test {
+    Token public token;
+
+    // function setUp() public {
+    //     nilla = new NillaToken(10000000);
+    // }
+
+    function testDeploy() public {
+        token = new Token(100000000);
+    }
+    // function testIncrement() public {
+    //     counter.increment();
+    //     assertEq(counter.number(), 1);
+    // }
+
+    // function testSetNumber(uint256 x) public {
+    //     counter.setNumber(x);
+    //     assertEq(counter.number(), x);
+    // }
+}
